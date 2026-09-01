a4abf64fb7507311d2d48f3c127151fef87a0297
Ben Euto <95237190+bene01-git@users.noreply.github.com>
2026-09-01 15:00:18 +0000

    edited title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("Select Page")
+st.title("IST688 Lab")
 
 page1 = st.Page('Lab1.py', title="Lab 1")
 page2 = st.Page('Lab2.py', title="Lab 2", default=True)
